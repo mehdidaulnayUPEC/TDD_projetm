1165862661107d1891a4a955c00c73dc6d17e17a
mehdidaulnayUPEC <chakermehdi7@gmail.com>
2023-10-08 21:34:08 +0200

    commit question 1B

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,8 +1,8 @@
 class FizzBuzz:
-    def affiche(self, nombre):
+    def affiche(self, n):
         resultat=""
 
-        for i in range (1, 101):
+        for i in range (1, n + 1):
             if i%15 == 0 :
                 resultat += "FrisBee"
             elif i%5 == 0 :
--- Test_FizzBuzz.py
+++ Test_FizzBuzz.py
@@ -8,9 +8,10 @@ class Test_FizzBuzz(unittest.TestCase):
 
 
     def test_affiche(self):
-        self.assertEqual(self.instance.affiche(10), "12Fizz4BuzzFizz78FizzBuzz")
-        self.assertEqual(self.instance.affiche(5), "12Fizz4Buzz")
+        self.assertEqual(self.instance.affiche(15), "12Fizz4BuzzFizz78FizzBuzz11Fizz1314FrisBee")
+        
 
 
 if __name__ == '__main__':
     unittest.main()
+
